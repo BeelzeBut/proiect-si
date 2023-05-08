5a0db64010e1cf703cab761d6928e109bf7d32b5
BeelzeBut <marco.buga@student.upt.ro>
2023-05-09 00:52:47 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
+
+type Pin = {
+  id: number;
+  state: boolean;
+}
 
 function App() {
+  // Set the initial state of the pins
+  const [pins, setPins] = useState(() => {
+    let pins: Pin[] = [];
+    for(let i = 0; i < 40; i ++) {
+      pins.push({
+        id: i,
+        state: true,
+      })
+    }
+
+    return pins;
+  });
+
+  // Function to toggle the color of a pin
+  const handlePinClick = (id: any) => {
+    setPins(prevPins => prevPins.map(pin => {
+      if (pin.id === id) {
+        return { ...pin, color: pin.state = !pin.state };
+      }
+      return pin;
+    }));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={'raspberry-pi'}>
+        <h1 style={{opacity: 0}}>Raspberry Pi Model</h1>
+        <div className="pin-container">
+          {pins.map(pin => (
+              <div
+                  key={pin.id}
+                  className={`pin pin-${pin.id} pin-${pin.state ? 'green' : 'red'}`}
+                  onClick={() => handlePinClick(pin.id)}
+              />
+          ))}
+        </div>
+      </div>
   );
 }
 
